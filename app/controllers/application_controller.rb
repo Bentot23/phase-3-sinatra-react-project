8388d1c96b2d4024f5a6fea2df213b00aecaabea
Bentot23 <92287648+Bentot23@users.noreply.github.com>
2023-01-21 01:22:06 -0600

    add post, patch and delete routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,31 @@ class ApplicationController < Sinatra::Base
     member.to_json
   end
 
+  post "/addmember" do
+    member = Member.create(
+      name: params[:name],
+      age: params[:age],
+      hometown: params[:hometown],
+      chapter_id: params[:chapter_id]
+    )
+    member.to_json
+  end
+
+  patch '/members/:id' do
+    member = Member.find(params[:id])
+    member.update(
+      name:params[:name]
+      age: params[:age],
+      hometown: params[:hometown],
+      chapter_id: params[:chapter_id]
+    )
+    member.to_json
+  end
+
+  delete '/members/:id' do
+    member = Member.find(params[:id])
+    member.destroy
+    member.to_json
+  end
+
 end
